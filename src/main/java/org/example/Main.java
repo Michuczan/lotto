71e871f05610be805127ab907ed0b991bf09574b
michuczan <michuczan@gmail.com>
2026-09-08 11:21:24 +0200

    10 losowych liczb z hashsetem

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,22 +1,20 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
         Random random = new Random();
-        ArrayList<Integer> listaLiczbLosowych = new ArrayList<>();
+        HashSet<Integer> zbiorLiczbLosowych = new HashSet<>();//zbior elementy bez powtorzen nieindeksowane
         int liczba;
         int i = 0;
-        while (listaLiczbLosowych.size()<10) {
+        while (zbiorLiczbLosowych.size()<10) {
             liczba = random.nextInt(15)+1;
-            if(listaLiczbLosowych.contains(liczba)){
-                continue;
-            }
-            listaLiczbLosowych.add(liczba);
+            zbiorLiczbLosowych.add(liczba);
         }
         System.out.println("Tablia liczb losowych");
-        System.out.println(listaLiczbLosowych);
+        System.out.println(zbiorLiczbLosowych);
     }
 }
