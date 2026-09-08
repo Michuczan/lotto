b4c667bc3c4f449651e282e8519340663e079eb4
michuczan <michuczan@gmail.com>
2026-09-08 11:12:32 +0200

    wylosowanie 10 liczb na arrayu

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,32 +1,22 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
         Random random = new Random();
-        int[] tablicaLiczbLosowych = new int[10];// nie mozna pozniej zmienic rozmiaru tablicy
+        ArrayList<Integer> listaLiczbLosowych = new ArrayList<>();
         int liczba;
-        boolean zawiera = false;
         int i = 0;
-        while (i<10) {
+        while (listaLiczbLosowych.size()<10) {
             liczba = random.nextInt(15)+1;
-            for (int j = 0; j < i; j++) {
-                if(liczba == tablicaLiczbLosowych[j]) {
-                    zawiera = true;
-                    break;
-                }
+            if(listaLiczbLosowych.contains(liczba)){
+                continue;
             }
-            if(!zawiera){
-                tablicaLiczbLosowych[i] = liczba;
-                i++;
-            } else {
-                zawiera = false;
-            }
+            listaLiczbLosowych.add(liczba);
         }
         System.out.println("Tablia liczb losowych");
-        for(int elementTablicy:tablicaLiczbLosowych) {
-            System.out.print(elementTablicy + " ");
-        }
+        System.out.println(listaLiczbLosowych);
     }
 }
