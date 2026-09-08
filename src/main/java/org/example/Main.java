fbccc07f1230c64f500d7b608a2395d7e0bb8983
michuczan <michuczan@gmail.com>
2026-09-08 10:24:57 +0200

    wylosowanie tablicy 10 elementowej

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,17 @@
 package org.example;
+
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
+        Random random = new Random();
+        int[] tablicaLiczbLosowych = new int[10];// nie mozna pozniej zmienic rozmiaru tablicy
+        for (int i = 0; i < tablicaLiczbLosowych.length; i++) {
+            tablicaLiczbLosowych[i] = random.nextInt(100)+1;
+        }
+        System.out.println("Tablia liczb losowych");
+        for(int elementTablicy:tablicaLiczbLosowych) {
+            System.out.print(elementTablicy + " ");
         }
     }
 }
