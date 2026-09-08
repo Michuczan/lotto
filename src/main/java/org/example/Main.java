31404bfc9b6b8e6db2deca4e72724efd5e7b13cf
michuczan <michuczan@gmail.com>
2026-09-08 11:07:08 +0200

    wylosowanie 10 elemntowej tablicy bez powtorzen

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,8 +6,23 @@ public class Main {
     public static void main(String[] args) {
         Random random = new Random();
         int[] tablicaLiczbLosowych = new int[10];// nie mozna pozniej zmienic rozmiaru tablicy
-        for (int i = 0; i < tablicaLiczbLosowych.length; i++) {
-            tablicaLiczbLosowych[i] = random.nextInt(100)+1;
+        int liczba;
+        boolean zawiera = false;
+        int i = 0;
+        while (i<10) {
+            liczba = random.nextInt(15)+1;
+            for (int j = 0; j < i; j++) {
+                if(liczba == tablicaLiczbLosowych[j]) {
+                    zawiera = true;
+                    break;
+                }
+            }
+            if(!zawiera){
+                tablicaLiczbLosowych[i] = liczba;
+                i++;
+            } else {
+                zawiera = false;
+            }
         }
         System.out.println("Tablia liczb losowych");
         for(int elementTablicy:tablicaLiczbLosowych) {
